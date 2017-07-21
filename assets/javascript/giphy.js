bf652664147a040891d7df20a3c28adcbdb5d2fe
David Jagodowski <davejag@comcast.net>
2017-07-21 17:01:09 -0400

    API call is working, but only pulls 1 gif

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,47 +2,52 @@
 
 $(document).ready(function(){
    
-  //Initial Array of fish
-  var fishArray = ["Rainbow Trout", "Brook Trout", "Brown Trout", "Smallmouth Bass"];
- // var fishDiv = $('<div id = "buttons-view">'); //create a new div
- 
- /*   function createDiv() {
-    	var myDiv = $("<div class='newDiv'>");
-    	var sampleText = "<p>I am writing this into my new Div</p>";
-    	myDiv.append(sampleText);
-
-    createDiv();
-  }
-*/    
+  //Create an initial array of fish
+  var fishArray = ["Trout", "Salmon", "Bass", "Catfish"];
   
-    // Function for displaying movie data
-    function renderButtons() {
-      //var newDiv = $("<div class='buttons-view'>"); //create a new div
-      // Deleting the existing fish prior to adding new one
-      // (otherwise you will have repeat buttons)
-      $("#buttons-view").empty();
 
-      // Creating a div to hold the new fish buttons.
-      
-      // Looping through the array of fish species.
+  //Create a new div to contain the buttons
+  //Assign a variable to a new div element.
+  var buttonsDiv = $("<div id='buttonsView'>");
+  //create sample text for testing
+  var titleText = "Click a button to view animaged GIFs!";
+  //create an element to hold the new buttons.
+  var p1 = $("<p>").text(titleText);
+  //insert sample text indo new div
+  buttonsDiv.append(p1);
+  //insert new div into DOM
+  $("#container").prepend(buttonsDiv);
+
+  //Create another div to contain the gif images 
+  var gifPlace = $("<div id='gifView'>");
+  //Places this new div after the buttonsView div.
+  $("#buttonsView").append(gifPlace);
+
+
+
+  // Function for displaying new buttons
+  function renderButtons() {
+    // Deleting the existing fish from the div prior to adding new one
+    // (otherwise you will have repeat buttons)
+    $("#buttonsView").empty(); 
+
+  // Loop through the array of fish species.
       for (var i = 0; i < fishArray.length; i++) {
         console.log(fishArray[i]);
-       // Then dynamicaly generating buttons for each fish in the array
-       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        console.log(a);
+       // Then dynamicaly generate buttons for each fish in the array
+        var b = $("<button>"); //jQuery generates beginning and end <button> tags.
         // Adding a class of fishClass to our button
-        a.addClass("fishClass");
+        b.addClass("fishClass");
         // Adding an attribute called "data-name" 
-        a.attr("data-name", fishArray[i]);
-        // Providing the initial button text
-        a.text(fishArray[i]);
+        b.attr("data-name", fishArray[i]);
+        // Providing the initial button text`
+        b.text(fishArray[i]);
         // Adding the button to the fishButtons div
-        $("#buttons-view").append(a);
+        $("#buttonsView").append(b);
       } // end for loop
     }
 
-    // Create an event handler for when the fish button is clicked
+    // Create an event handler for when a fish button is clicked
       $("#addFish").on("click", function(event) {
       	//prevent the Submit button from submitting a form.
         event.preventDefault();
@@ -55,6 +60,65 @@ $(document).ready(function(){
         // Calling renderButtons which handles the processing of our movie array
         renderButtons();
       });
+
+      // Adding a click event listener to all elements (i.e. buttons) with a class of "fishClass"
+      $(document).on("click", ".fishClass", displayGifs);
+      
+
+// ---------------------------------------------------------------//
+
+      // displayGifs function re-renders the HTML to display the appropriate content
+      function displayGifs() {
+        console.log("function displayGifs is called.");
+
+        var gifClip = $(this).attr("data-name"); // "data" is the name of the object from GIPHY API
+        var apiKey = "72d9ab225f604dd886525d7f63eb5f65"; //my personal API key
+        var gifLimit = 10; //number of gif files to retrieve
+        var gifRating = "g"; //filters results by the specified rating.
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+gifClip+"&apikey="+apiKey+"&limit="+gifLimit;
+      console.log("URL is: " + queryURL);
+        // Creating an AJAX call for the specific button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+ console.log(response);
+          // Creating a div to hold the gifs
+          var gifDiv = $("<div class='gifClass'>");
+
+          // Storing the rating data
+          var rating = response.data[0].rating;
+
+          // Creating an element to have the rating displayed
+          var pOne = $("<p>").text("Rating: " + rating);
+
+          // Displaying the rating
+          gifDiv.append(pOne);
+
+           var gifType = response.data[0].type 
+           console.log("gifImg = " + gifType);
+           var addType = $("<p>").text("Type is: " + gifType);
+           gifDiv.prepend(addType);
+           
+           var gifImg = response.data[0].images.fixed_height_still.url;
+           console.log("source = " + gifImg);
+           var addImg = $("<img>").attr("src", gifImg);
+           gifDiv.prepend(addImg);
+
+/*          // Retrieving the URL for the image
+          var gifURL = response.data[0].url;  //From GIPHY docs. Not 100% sure of this.
+console.log("gifURL = " + gifURL);
+          // Creating an element to hold the image
+          var addLink = $("<img>").attr("src", gifURL);
+
+          // Appending the image
+          gifDiv.append(addLink);
+*/
+          // Putting the gif above the previous movies
+          $("#container").append(gifDiv);
+        });
+
+      }
 
  // Calling the renderButtons function to display the intial buttons
       renderButtons();
